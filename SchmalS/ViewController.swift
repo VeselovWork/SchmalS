ed445154ecea1013aa6b4d5945066dfc902132f6
AW <veselovwork@gmail.com>
2021-07-28 23:39:20 +0300

    [develop] - ViewController: Create Switch

--- SchmalS/ViewController.swift
+++ SchmalS/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mySwitch = UISwitch()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        view.backgroundColor = .green
+        
+        //Create Switch
+        self.mySwitch.frame = CGRect(x: 100, y: 100, width: 0, height: 0)
+        self.view.addSubview(self.mySwitch)
+        
     }
 
 
